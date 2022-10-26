2de3bdcd55f11b3f11ee56e545c562d0e816c3ba
Malvina Hasa <hasamalvina24@gmail.com>
2022-10-26 18:36:20 -0400

    added inquirer package and other dependecies on index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// DEPENDENCIES
+// npm
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+// JS files
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
